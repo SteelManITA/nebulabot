50ed2f3a1ef9746798c41d9b453e08ade123d8ec
Hersel Giannella <hersel1991@gmail.com>
2019-07-08 13:57:28 +0200

    Add files via upload

---
+++ utils/decorator.py
@@ -0,0 +1,25 @@
+#DEFINIZIONE COMANDI ADMIN
+from functools import wraps
+
+
+LIST_OF_ADMINS = [605363037,53451006,77582926,442991919]
+
+def restricted(func):
+    @wraps(func)
+    def wrapped(bot, update):
+        user_id = update.effective_user.id
+        if user_id not in LIST_OF_ADMINS:
+            print("Unauthorized access denied for {}.".format(user_id))
+            return
+        return func(bot, update)
+    return wrapped
+
+#CANCELLA COMANDI
+def cancellacomandi(func):
+    @wraps(func)
+    def wrapped(bot, update):
+        if update.message.text is not None:
+          if update.message.text.startswith("/"):
+            bot.delete_message(update.message.chat_id, update.message.message_id)
+        return func(bot, update)
+    return wrapped
