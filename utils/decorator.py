faeb78e64c90013e301554ebfd9009a527472036
Hersel Giannella <hersel1991@gmail.com>
2019-07-11 00:23:48 +0200

    Add files via upload

--- utils/decorator.py
+++ utils/decorator.py
@@ -2,7 +2,7 @@
 from functools import wraps
 
 
-LIST_OF_ADMINS = [123456789,123456789]
+LIST_OF_ADMINS = [605363037,650789883,70720440,481465978,204641236,172042287]
 
 def restricted(func):
     @wraps(func)
@@ -21,5 +21,17 @@ def cancellacomandi(func):
         if update.message.text is not None:
           if update.message.text.startswith("/"):
             bot.delete_message(update.message.chat_id, update.message.message_id)
+        return func(bot, update)
+    return wrapped
+#OWNERBOT
+OWNER_LIST= [605363037]
+
+def ownerbot(func):
+    @wraps(func)
+    def wrapped(bot, update):
+        user_id = update.effective_user.id
+        if user_id not in OWNER_LIST:
+            print("Unauthorized access denied for {}.".format(user_id))
+            return
         return func(bot, update)
     return wrapped
