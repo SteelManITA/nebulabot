383976e33cd3ed65d8d45ad51ed124a953865ae9
Hersel Giannella <hersel1991@gmail.com>
2019-07-13 13:39:47 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 bot_token = "INSERT TOKEN HERE"
 admingroup_id = -123456789
 channel_id = -123456789 # private channel for reports
-annuncio_group = [-123456789,-123456789] 
+annuncio_group = [-123456789,-123456789]
+database = {
+  'server': 'localhost',
+  'user': 'user',
+  'password': 'password',
+  'name': 'databasename',
+}
