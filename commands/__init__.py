54d9710266ad1d41b63afb0c05e1b85ac5546a52
Hersel Giannella <hersel1991@gmail.com>
2019-07-14 21:38:04 +0200

    Add files via upload

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ["muta", "smuta", "aiuto", "start", "regole", 
 "ban", "io", "source", "distro", 
-"fissa", "say", "annuncio", "richiedifunzione", "leave", "chatdescription", "chattitle", "insertbattuta"]
+"fissa", "say", "annuncio", "richiedifunzione", "leave", "chatdescription", "chattitle", "insertbattuta", "insertcustomhandler"]
 
 from commands import *
---
+++ commands/insertcustomhandler.py
@@ -0,0 +1,26 @@
+import config
+import MySQLdb
+from utils import decorator
+
+@decorator.restricted
+@decorator.cancellacomandi
+def init(bot, update):
+    if update.message.reply_to_message is not None:
+        if str(update.message.text).lower().startswith("/setrisposta"):
+            var_domanda = update.message.reply_to_message.text
+            var_risposta = update.message.text
+            var_risposta = update.message.text[12:]
+            
+            db=MySQLdb.connect(config.database['server'],config.database['user'],config.database['password'],config.database['name'])
+   
+            db.autocommit(True)
+            cur=db.cursor()
+            
+ 
+   
+            cur.execute('INSERT INTO risposte (domanda_text, risposta_text) VALUES ("'+var_domanda+'","'+var_risposta+'")')
+            
+        
+   
+            cur.close()
+            db.close()
