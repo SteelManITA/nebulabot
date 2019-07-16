e0861da97d0e2578d2aded8783529adc605e46f6
Hersel Giannella <hersel1991@gmail.com>
2019-07-16 18:30:16 +0200

    Add files via upload

---
+++ commands/server.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import psutil
+from utils import decorator
+
+@decorator.ownerbot
+@decorator.cancellacomandi
+def init(bot, update):
+    bot.send_message(update.message.chat_id,text="<b>SERVER STATUS</b>\n<b>CPU:</b> <code>{cpu}%</code>\n<b>RAM:</b> <code>{ram}%</code>\n=========\nantares.hersel.it"
+        .format(cpu=psutil.cpu_percent(),ram=psutil.virtual_memory()[2]),
+        parse_mode='HTML')
