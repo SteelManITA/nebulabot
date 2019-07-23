2216e8bb5ab1b2216a3cbb402032cb9a09c34344
Hersel Giannella <hersel1991@gmail.com>
2019-07-23 23:06:52 +0200

    Add files via upload

--- dialogs/customhandler.py
+++ dialogs/customhandler.py
@@ -5,18 +5,23 @@ import MySQLdb
  
 def init(bot, update):
 	if update.message.text is not None:
-		
-			db=MySQLdb.connect(config.database['server'],config.database['user'],config.database['password'],config.database['name'])
-   
+		if str(update.message.text).lower().startswith("nebula"):
+			db=MySQLdb.connect(
+                config.database['server'],
+                config.database['user'],
+                config.database['password'],
+                config.database['name']
+            )
 			db.autocommit(True)
+			db.set_character_set('utf8mb4')
 			cur=db.cursor()
- 
-   
-			cur.execute("SELECT risposta_text FROM risposte WHERE domanda_text = '{domanda}'".format(domanda = update.message.text))
-			row=cur.fetchone()
+  		
+			query = 'SELECT risposta_text FROM risposte WHERE domanda_text = %s'
+			cur.execute(query, [update.message.text])
+			row = cur.fetchone()
+			
 			while row is not None:
-				bot.send_message(update.message.chat_id, text=row[0],parse_mode='HTML')
+				bot.send_message(update.message.chat_id, text=row[0], parse_mode='HTML')
 				row = cur.fetchone()
-   
 				cur.close()
 				db.close()
