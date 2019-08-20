afab27c4e22e0925101911acc9bf0317b263ee3f
Hersel Giannella <hersel1991@gmail.com>
2019-08-20 14:24:55 +0200

    Add files via upload

--- commands/comunicazione.py
+++ commands/comunicazione.py
@@ -7,6 +7,6 @@ def init(bot, update):
 	if update.message.text is not None:
 		if update.message.text.lower().startswith("/comunicazione"):
 			var_messaggio = update.message.text[14:]
-			bot.send_message(config.admingroup_id, text="<b>Comunicazione</b>\n\n{}\n@Hersy @TheLonelyAdventurer @jfet97 @SteelManITA @iAmGio @Folgore796"
+			bot.send_message(config.admingroup_id, text="<b>Comunicazione</b>\n\n{}\n@Hersy @TheLonelyAdventurer @jfet97 @thecmo @SteelManITA @iAmGio @Folgore796"
                 .format(var_messaggio), parse_mode='HTML')
 			bot.pin_chat_message(update.message.chat_id, update.message.message_id+1)
