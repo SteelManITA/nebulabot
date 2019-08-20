534495b483f3102acc2797b3f9bf738cbdae5b0b
Hersel Giannella <hersel1991@gmail.com>
2019-08-20 14:25:13 +0200

    Add files via upload

--- commands/community.py
+++ commands/community.py
@@ -10,7 +10,8 @@ def init(bot, update):
     InlineKeyboardButton("JAVASCRIPT", url='https://t.me/javascript_ita'),
     InlineKeyboardButton("PROGRAMMAZIONE", url='https://t.me/programmazione_ita'),
     InlineKeyboardButton("PHP", url='https://t.me/php_italia'),
-    InlineKeyboardButton("JAVA", url='https://t.me/java_italia')
+    InlineKeyboardButton("JAVA", url='https://t.me/java_italia'),
+    InlineKeyboardButton("OFFTOPIC", url='https://t.me/offtopic_ita')
     ]
     reply_markup = InlineKeyboardMarkup(util.build_menu(button_list, n_cols=2))
-    bot.send_message(update.message.chat_id, text="Le Nostre Community", reply_markup=reply_markup)
+    bot.send_message(update.message.chat_id, text="Le Nostre Community Telegram", reply_markup=reply_markup)
