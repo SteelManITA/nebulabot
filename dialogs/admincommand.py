b1e992178c2c62525c3a872416726ee3a62daa24
Hersel Giannella <hersel1991@gmail.com>
2019-08-24 17:44:14 +0200

    Update admincommand.py

--- dialogs/admincommand.py
+++ dialogs/admincommand.py
@@ -12,13 +12,19 @@ def init(bot, update):
 			var_messaggio = update.message.text[7:]
 			bot.send_message(config.admingroup_id, 
                  text="<b>NUOVA RICHIESTA DI SUPPORTO</b>\nChat: {idchat}\nAutore: {username}\nLink: {linkurl}{idchat}/{link}\n<code>Messaggio:{}</code>"
-					 .format(var_messaggio, username="@"+update.message.from_user.username, link=update.message.message_id, 
-					 idchat=update.message.chat.title, linkurl="https://t.me/"),
-                 parse_mode='HTML', reply_markup=reply_markup)
+					 .format(var_messaggio, username="@"+update.message.from_user.username, 
+					 link=update.message.message_id, 
+					 idchat=update.message.chat.title, 
+					 linkurl="https://t.me/"),
+                 parse_mode='HTML', 
+				 reply_markup=reply_markup)
 			bot.send_message(config.channel_id, 
                  text="<b>NUOVA RICHIESTA DI SUPPORTO</b>\nChat: {idchat}\nAutore: {username}\nLink: {linkurl}{idchat}/{link}\n<code>Messaggio:{}</code>"
-					 .format(var_messaggio, username="@"+update.message.from_user.username, link=update.message.message_id, 
-					 idchat=update.message.chat.title, linkurl="https://t.me/"),
+					 .format(var_messaggio, 
+					 username="@"+update.message.from_user.username, 
+					 link=update.message.message_id, 
+					 idchat=update.message.chat.title, 
+					 linkurl="https://t.me/"),
                  parse_mode='HTML')
 
 def risolto(bot, update):
@@ -26,4 +32,7 @@ def risolto(bot, update):
 	var_messaggio = query.message.text
 	var_messaggio = query.message.text[6:]
 	query.edit_message_text(text="{}\n<b>Risolto da: @{username}</b>"
-	.format(var_messaggio,username=str(update.effective_user.username)),parse_mode='HTML')
+	.format(var_messaggio,
+	username=str(update.effective_user.username)),
+	parse_mode='HTML')
+
